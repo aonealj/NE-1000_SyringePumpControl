bb2168ba9d15aaa7efac15ffe5ccb58a2cbf4f47
aonealj <35272176+aonealj@users.noreply.github.com>
2021-05-02 19:42:47 -0400

    Simplified functions.

--- NE-1000 Syringe Pump Control.py
+++ NE-1000 Syringe Pump Control.py
@@ -2,35 +2,40 @@
 
 import serial
 
+# setup for communications
+def setup_serial(port_name, baud_rate, time_out):
+    com_port = serial.Serial()
+    com_port.port = port_name
+    com_port.baudrate = baud_rate
+    com_port.timeout = time_out
+    com_port.write_timeout = time_out
+    return com_port
+
+
 # properly format input for the syringe pumps
 def cmdsp(inputChar2):
-    stringInput2 = str(inputChar2) + '\r'
-    return bytes(stringInput2, 'ascii')
+    return bytes(str(inputChar2) + '\r', 'ascii')
 
 
 # command to change the diameter
 # keep space between command code and associated inputs
-def setDIAcmd(dia):                         #units are mm
-    DIAstr = 'DIA ' + str(dia)
-    return cmdsp(DIAstr)
+def setDIAcmd(dia):  # units are mm
+    return cmdsp('DIA ' + str(dia))
 
 
 # command to select phase num
 def selPHN(phaseNum):
-    phaseNumStr = 'PHN ' + str(phaseNum)
-    return cmdsp(phaseNumStr)
+    return cmdsp('PHN ' + str(phaseNum))
 
 
 # set function to pumping rate type
 def funcRate():
-    funcRateStr = 'FUN RAT'
-    return cmdsp(funcRateStr)
+    return cmdsp('FUN RAT')
 
 
 # set rate w/correct units
 def pumpRate(pumping_rate, pumping_units):
-    pumpRateStr = 'RAT ' + str(pumping_rate) + ' ' + str(pumping_units)
-    return cmdsp(pumpRateStr)
+    return cmdsp('RAT ' + str(pumping_rate) + ' ' + str(pumping_units))
 
 
 # set rate w/o units (no units [nu])
@@ -40,30 +45,25 @@ def pump_rate_nu(pumping_rate):
 
 # function to set the volume
 def setVol(pumpVol):
-    pumpVolStr = 'VOL ' + str(pumpVol)
-    return cmdsp(pumpVolStr)
+    return cmdsp('VOL ' + str(pumpVol))
 
 
 # function to set the volume units--this cannot be combined with the setting the units
 # see the manual for the valid inputs
 def setVolUnits(pumpVolUnits):
-    pumpVolUnitsStr = 'VOL ' + str(pumpVolUnits)
-    return cmdsp(pumpVolUnitsStr)
+    return cmdsp('VOL ' + str(pumpVolUnits))
 
 
 # function for cmd to set pump direction
 def setDir(pumpDir):
-    pumpDirStr = 'DIR ' + str(pumpDir)
-    return cmdsp(pumpDirStr)
+    return cmdsp('DIR ' + str(pumpDir))
 
 
 # function for cmd to input stop set
-#probably not necessary
+# probably not necessary
 def setStp():
-    stpStr = 'FUN STP'
-    return cmdsp(stpStr)
+    return cmdsp('FUN STP')
 
 
 def runPrgmNum(PHS_num):
-    PHS_num_str = 'RUN ' + str(PHS_num)
-    return cmdsp(PHS_num_str)
+    return cmdsp('RUN ' + str(PHS_num))
